cb198dae063bc5c11187c059cac942d6a07de08e
rthompson458 <rthompson458@inqservices.com>
2026-08-14 12:40:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ resource "helm_release" "mongodb_provisioning" {
   chart     = "./mongodb-chart"
   namespace = "mongodb"
 
-values = [
-  yamlencode({
-    mongodbDatabases    = var.mongodb_databases
-    mongodbRequestUsers = var.mongodb_request_users
-  })
-]
+  values = [
+    yamlencode({
+      mongodbDatabases    = var.mongodb_databases
+      mongodbRequestUsers = var.mongodb_request_users
+    })
+  ]
+}
