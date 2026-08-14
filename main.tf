4ff36f02812b920a1ecfe4913b73fc012877e113
rthompson458 <rthompson458@inqservices.com>
2026-08-14 09:17:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "helm_release" "spacelift_helm_test" {
-  name      = "spacelift-helm-test"
-  chart     = "./chart"
-  namespace = "default"
-}
-
 resource "helm_release" "mongodb_provisioning" {
   name      = "mongodb-provisioning"
   chart     = "./mongodb-chart"
