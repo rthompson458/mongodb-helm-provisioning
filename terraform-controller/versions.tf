5010ea38b955b833d54762d1385075fbc3cfe454
rthompson458 <rthompson458@inqservices.com>
2026-09-04 12:32:16 -0400

    Add terraform-controller/versions.tf

---
+++ terraform-controller/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  backend "kubernetes" {}
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.2"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.9"
+    }
+
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 5.11"
+    }
+  }
+}
