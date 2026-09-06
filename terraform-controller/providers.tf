1c2bba2138ad6298abf68f2f70210ab22321eecd
rthompson458 <rthompson458@inqservices.com>
2026-09-06 13:06:07 -0400

    Remove old terraform-controller/providers.tf after terraform-dbaas rename

--- terraform-controller/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "vault" {
-  address = var.vault_address
-}
-
-provider "kubernetes" {
-  config_path    = pathexpand(var.kubeconfig_path)
-  config_context = var.kube_context != "" ? var.kube_context : null
-}
